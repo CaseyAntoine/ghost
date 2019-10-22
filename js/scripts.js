ad12004e685ca132515f77d844cb972386a85468
Student <example@example.com>
2019-10-22 14:48:54 -0700

    updated css styling, updating jQuery functions.

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,36 @@ $(document).ready(function() {
 
     $("#initially-showing").toggle();
     $(".initially-hidden").toggle();
+    $("body").toggleClass("blackout");
+    $(".jumbotron").toggleClass("dark");
   });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("whiteout");
+    $(".jumbotron").removeClass();
+    $(".jumbotron").addClass("whiteout");
+    $("img").removeClass();
+    $("img").addClass("whiteoutimage");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue");
+    $(".jumbotron").removeClass();
+    $(".jumbotron").addClass("blue");
+    $("img").removeClass();
+    $("img").addClass("whiteoutimage");
+
+    });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red");
+    $(".jumbotron").removeClass();
+    $(".jumbotron").addClass("red");
+    $("img").removeClass();
+    $("img").addClass("whiteoutimage");
+  });
+
 });
